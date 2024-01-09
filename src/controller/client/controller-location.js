5219d6738ab856c7afc602f58fbada130904d642
duy366110 <duy366110@gmail.com>
2024-01-09 17:46:53 +0700

    #Commit service get location.

--- src/controller/client/controller-location.js
+++ src/controller/client/controller-location.js
@@ -4,6 +4,37 @@ class ControllerLocation {
 
     constructor() { }
 
+    // TRUY XUẤT AMOUNT LOCATION
+    async getLocationAmount(req, res, next) {
+        await ServiceLocation.getAmount((information) => {
+            let { status, message, amount } = information;
+            if(status) {
+                return res.status(200).json({status, message, amount});
+            }
+            return res.status(400).json({status, message, amount: 0});
+        })
+    }
+
+    // LẤY DANH SÁCH LOCATION
+    async getLocations (req, res, next) {
+        try {
+            let { limit, start} = req.params;
+            await ServiceLocation.getLimit(limit, start, (information) => {
+                let { status, message, locations, error } = information;
+                if(status) {
+                    res.status(200).json({status: true, message, locations});
+
+                } else {
+                    res.status(406).json({status: false, message, error});
+                }
+            });
+
+        } catch (error) {
+            // PHƯƠNG THỨC LỖI
+            res.status(500).json({status: false, message: 'Internal server failed'});
+        }
+    }
+
     // TRUY XUẤT DANH MỤC LOCATION
     async getLocationAll( req, res, next) {
         try {
--- src/router/client/router-location.js
+++ src/router/client/router-location.js
@@ -3,6 +3,12 @@ const router = require('express').Router();
 const ControllerLocation = require("../../controller/client/controller-location");
 
 // ROUTER TRUY XUẤT DANH MỤC LOCATION
+router.get("/amount", ControllerLocation.getLocationAmount);
+
+// ROUTER PAGINATION LẤY THÔNG TIN DANH SÁCH LOCATION
+router.get("/:limit/:start", ControllerLocation.getLocations);
+
+// ROUTER TRUY XUAT ALL LOCATION
 router.get("/", ControllerLocation.getLocationAll);
 
 module.exports = router;
